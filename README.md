f22a7e745d3c62caea1bec3a588f8ca49d26b6ac
christiansiegel <christiansiegel@users.noreply.github.com>
2017-03-30 08:42:46 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# nabto-android-jcenter-demo
+# Nabto Android JCenter Demo
+
+Simplest possible example of an Android app that uses the [Android Client API](https://github.com/nabto/android-client-api) from [JCenter](https://bintray.com/nabto/android/com.nabto.android%3Anabto-api).
+
+All the Nabto action happens in [MainActivity.java](app/src/main/java/com/nabto/androidjcenterdemo/MainActivity.java). The RPC result is printed to the log and displayed on the UI instead of the initial *Hello World!*.
---
+++ app/src/main/java/com/nabto/androidjcenterdemo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.nabto.androidjcenterdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.nabto.api.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final String INTERFACE_XML =
+            "<unabto_queries>" +
+            "  <query name='wind_speed.json' id='2'>" +
+            "    <request></request>" +
+            "    <response format='json'>" +
+            "      <parameter name='rpc_speed_m_s' type='uint32'/>" +
+            "    </response>" +
+            "  </query>" +
+            "</unabto_queries>";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NabtoApi api = new NabtoApi(new NabtoAndroidAssetManager(this));
+        api.startup();
+
+        Session session = api.openSession("guest", "");
+        if (session.getStatus() == NabtoStatus.OK) {
+
+            RpcResult result = api.rpcSetDefaultInterface(INTERFACE_XML, session);
+            if(result.getStatus() != NabtoStatus.OK) {
+                // handle error
+            }
+
+            result = api.rpcInvoke("nabto://demo.nabto.net/wind_speed.json?", session);
+            if (result.getStatus() == NabtoStatus.OK) {
+                Log.v("demo", result.getJson());
+                ((TextView) findViewById(R.id.textView)).setText(result.getJson());
+            }
+
+            api.closeSession(session);
+        }
+
+        api.shutdown();
+    }
+}

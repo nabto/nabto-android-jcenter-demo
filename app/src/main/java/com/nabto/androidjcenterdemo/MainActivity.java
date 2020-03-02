d8839cf12e07035802d9aa1c4c8fc3fdca236f98
Ulrik Gammelby <ug@nabto.com>
2020-03-02 15:06:15 +0100

    http client works

--- app/src/main/java/com/nabto/androidjcenterdemo/MainActivity.java
+++ app/src/main/java/com/nabto/androidjcenterdemo/MainActivity.java
@@ -7,6 +7,11 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.nabto.api.NabtoAndroidAssetManager;
 import com.nabto.api.NabtoApi;
@@ -121,24 +126,19 @@ public class MainActivity extends AppCompatActivity {
 
     private void performHttpRequest(int port) {
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.google.com";
-
-// Request a string response from the provided URL.
+        String url ="http://127.0.0.1:" + port + "/";
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
+                        appendText("Got an HTTP response through tunnel: \n" + response);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
+                appendText("HTTP error: " + error);
             }
         });
-
-// Add the request to the RequestQueue.
         queue.add(stringRequest);
     }
 
